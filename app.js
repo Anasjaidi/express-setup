c61c2ea16d9f2be4ff5ed5548399a74a20ebafc2
anas jaidi <anas.jaidi@icloud.com>
2023-04-13 17:29:22 +0000

    dependes on http errors package

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 const express = require("express");
+const AppError = require("http-errors");
 const morgan = require("morgan");
 const cors = require("cors");
 const path = require("path");
-
+const dotenv = require("dotenv");
 
 // append .env vars to envirement variables
-dotenv.config('./.env.test')
+dotenv.config("./.env.test");
 
 // select settings for choosen mode
 const {
 	corsOptions,
-  name,
+	name,
 	app: { port, debug, logger_format },
 } = require("./config/settings")(env);
-const env = process.env.NODE_ENV
-
+const env = process.env.NODE_ENV;
 
 // create instance from express
 const app = express();
 
 // add some vars to express app
-app.set("port", port)
-app.set("debug", debug)
-app.set("env", name)
-
+app.set("port", port);
+app.set("debug", debug);
+app.set("env", name);
 
 // initialize middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan(process.env.MORGAN_MODE));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './public'), {dotfiles: true}))
+app.use(express.static(path.join(__dirname, "./public"), { dotfiles: true }));
+
+
+// start deafult route
+app.use('*', (req, res, next) => {
+  next(new AppError.NotFound(`resource requested ${req.baseUrl} not found.`))
+})
+
+// start error Gateway
+app.use()
