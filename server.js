760f5ec78f3e179174213277da6c81da94762275
anas jaidi <anas.jaidi@icloud.com>
2023-04-13 18:01:38 +0000

    fire the server

---
+++ server.js
@@ -0,0 +1,16 @@
+const app = require('./app')
+
+
+const port = app.get('port')
+const env = app.get('env')
+const debug = app.get('debug')
+
+// start listening on port specified
+server.listen(port);
+
+server.on("listening", () => {
+    console.log(`* Environment : ${env}`);
+    console.log(`* Debugger : ${debug ? "On" : "Off"}`);
+    console.log(`* Running on http://localhost:${port} (CTRL + C to quit)`);
+});
+
