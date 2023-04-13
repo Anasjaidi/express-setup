4aba6a213c13751a3707ee37cd93f651a9088278
anas jaidi <anas.jaidi@icloud.com>
2023-04-13 17:38:34 +0000

    add errors gateway

---
+++ errors/ErrorGateway.js
@@ -0,0 +1,15 @@
+
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500
+
+  err.status = err.status || "error"
+
+  if (req.app.get('env') === 'development') {
+
+  } else if (req.app.get('env') === 'testing') {
+
+  } else { // production
+
+  }
+}
