271d816fbab3904457244eea79c89014c625e6a6
anas jaidi <anas.jaidi@icloud.com>
2023-04-16 17:37:57 +0000

    finish user sig up schema

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const userController = require('../controllers/userController')
+const Joi = require('joi')
+const Validator = require('../helpers/JoiValidator')
 
 const router = express.Router()
 
-router.post('/signup', userController.signUp)
+const userSignUpSchema = Joi.object().keys({
+  firstName: Joi.string().min(3).max(50).required(),
+  lastName: Joi.string().min(3).max(50).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(5).required(),
+  repeatpassword: Joi.ref('passowrd')
+})
+
+router.post('/signup', Validator(userSignUpSchema) ,userController.signUp)
 router.post('/signin', userController.signIn)
 
 module.exports = router
